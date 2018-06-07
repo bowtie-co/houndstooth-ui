34cf87a8b0146c3e0fd76d10126619f7cf28cbdf
Tim <tbrandle53@gmail.com>
2018-06-07 16:44:49 -0600

    withForm, withFormatting, storage, api

---
+++ src/helpers/withForm.jsx
@@ -0,0 +1,40 @@
+import { compose, withHandlers, withState } from 'recompose'
+
+export default compose(
+  withState('formData', 'setFormData', {}),
+  withState('errors', 'handleErrors', {}),
+  withHandlers({
+    onChange: ({ formData, setFormData, handleChange }) => (e) => {
+      const key = e.target.name
+      let value = e.target.value
+
+      if (e.target.type === 'checkbox') {
+        value = e.target.checked
+      } else if (e.target.type === 'select-multiple') {
+        const selected = []
+
+        for (let i = 0; i < e.target.options.length; i++) {
+          if (e.target.options[i].selected) {
+            selected.push(e.target.options[i].value)
+          }
+        }
+        value = selected
+      }
+
+      const newData = Object.assign({}, formData, { [key]: value })
+
+      if (typeof handleChange === 'function') {
+        handleChange(e, newData)
+      }
+
+      setFormData(newData)
+    },
+    getErrors: ({ errors }) => (key) => {
+      if (key) {
+        return errors[key] || []
+      } else {
+        return errors
+      }
+    }
+  })
+)
---
+++ src/lib/storage.js
@@ -0,0 +1,41 @@
+/* global localStorage */
+
+import EventEmitter from 'eventemitter2'
+
+class Storage extends EventEmitter {
+  set (key, value) {
+    const flatValue = typeof value === 'object' ? JSON.stringify(value) : value
+
+    localStorage.setItem(key, flatValue)
+
+    this.emit('change', { key, value })
+    this.emit(`${key}_changed`, value)
+  }
+
+  get (key) {
+    const storedValue = localStorage.getItem(key)
+
+    try {
+      return JSON.parse(storedValue)
+    } catch (e) {
+      return storedValue
+    }
+  }
+
+  remove (key) {
+    localStorage.removeItem(key)
+
+    this.emit('remove', key)
+    this.emit(`${key}_removed`)
+  }
+
+  clear () {
+    localStorage.clear()
+
+    this.emit('clear')
+  }
+}
+
+const storage = new Storage()
+
+export default storage
