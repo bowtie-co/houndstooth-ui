90deef2e2a5f00cac30989189d3c045b64137ade
Tim <tbrandle53@gmail.com>
2018-06-20 11:13:16 -0600

    linting

--- src/components/organisms/TopNav/TopNav.jsx
+++ src/components/organisms/TopNav/TopNav.jsx
@@ -5,10 +5,10 @@ import { NavLink } from '../../atoms'
 const TopNav = (props) => {
   return (
     <section className='top-nav-section' >
-      <Header title={'Welcome to Bowtie\'s React-Recompose Starter Kit'}/>
+      <Header title={'Welcome to Bowtie\'s React-Recompose Starter Kit'} />
       <div className='nav-bar'>
-        <NavLink path={'/home'} title={'Home'}/>
-        <NavLink path={'/demo'} title={'Demo'}/>
+        <NavLink path={'/home'} title={'Home'} />
+        <NavLink path={'/demo'} title={'Demo'} />
       </div>
     </section>
   )
