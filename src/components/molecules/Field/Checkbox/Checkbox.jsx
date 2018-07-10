fdf5e791a280b8a978342dd2df32e6ac269b9db3
Tim Brandle <tim@bowtie.co>
2018-07-10 11:04:19 -0600

    manually passing in e value on Checkbox to turn 'on' 'off' values to true/false

--- src/components/molecules/Field/Checkbox/Checkbox.jsx
+++ src/components/molecules/Field/Checkbox/Checkbox.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Input } from 'reactstrap'
 import { FormGroup } from '../../../atoms'
 
-const Checkbox = ({ id, value, ...rest }) => {
+const Checkbox = ({ id, value, onChange, ...rest }) => {
   return (
     <FormGroup className='marLeft' {...rest}>
-      <Input id={id} type='checkbox' checked={value} {...rest} />
+      <Input id={id} type='checkbox' defaultChecked={value} onClick={() => onChange({ target: { value: !value } })} {...rest} />
     </FormGroup>
   )
 }
