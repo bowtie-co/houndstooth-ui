355b3e2e30ee435bd830ce231eb164af2b87e7db
Tim Brandle <tim@bowtie.co>
2018-07-11 17:26:34 -0600

    titlize labels in recursive fields

--- src/components/organisms/RecursiveFields/FieldSingle.jsx
+++ src/components/organisms/RecursiveFields/FieldSingle.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FieldContainer } from '../../molecules'
+import { titleize } from '@bowtie/utils'
 import FieldGroup from './FieldGroup'
 
 const FieldSingle = (props) => {
@@ -7,15 +8,15 @@ const FieldSingle = (props) => {
   const currentLocation = location === '' ? name : `${location}.${name}`
 
   if (Array.isArray(field)) {
-    return <FieldGroup title={name} fields={{}} location={currentLocation} handleChange={handleChange} />
+    return <FieldGroup title={titleize(name, '_')} fields={{}} location={currentLocation} handleChange={handleChange} />
   } else if (field && typeof field === 'object') {
-    return <FieldGroup title={name} fields={field} location={currentLocation} handleChange={handleChange} />
+    return <FieldGroup title={titleize(name, '_')} fields={field} location={currentLocation} handleChange={handleChange} />
   } else {
     return (
       <FieldContainer
         key={name}
         name={name}
-        label={name}
+        label={titleize(name, '_')}
         placeholder={name}
         value={field}
         onChange={(e) => handleChange(currentLocation, e.target.value)}
