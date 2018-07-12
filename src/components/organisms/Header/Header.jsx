ed5c1fdfee5e4d2021355daecbdfa5088167b9e9
Tim Brandle <tim@bowtie.co>
2018-07-12 14:51:18 -0600

    conditional for login logout button

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Brand } from '../../molecules'
 import { NavLink, Login, Logout } from '../../atoms'
+import auth from '../../../lib/auth'
+
 
 const Header = () => {
   return (
     <section className='top-nav-section' >
       <Brand title={'Houndstooth'} />
       <div className='nav-bar'>
-        <NavLink to={'/home'}>Home</NavLink>
         <NavLink to={'/repos/'}>Repos</NavLink>
-        <Login />
-        <Logout />
+        {
+          auth.isAuthenticated()
+            ? <Logout />
+            : <Login />
+        }
       </div>
     </section>
   )
