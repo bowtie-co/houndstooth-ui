b39b2748e3b606459fff24c0985b2afe22013ff4
Tim Brandle <tim@bowtie.co>
2018-07-12 17:22:26 -0600

    change name to value for display on dom

--- src/components/molecules/Field/FileUpload/FileUpload.jsx
+++ src/components/molecules/Field/FileUpload/FileUpload.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import FileBase64 from 'react-file-base64'
 import { titleize } from '@bowtie/utils'
 
-const FileUpload = ({ field, handleFileUpload, fileUrl, name }) => {
+const FileUpload = ({ value, handleFileUpload, fileUrl, name }) => {
   return (
     <div>
       <img style={{ maxWidth: '300px' }} src={fileUrl} alt='' />
       <p>{titleize(name, '_')}</p>
-      <p>{field}</p>
+      <p>{value}</p>
       <FileBase64
         multiple={false}
         onDone={handleFileUpload} />
