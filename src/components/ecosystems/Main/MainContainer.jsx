364d291b56f66d63657dac57defa3f2f58c46f1d
Tim Brandle <tim@bowtie.co>
2018-07-25 10:21:27 -0600

    pagination in main

--- src/components/ecosystems/Main/MainContainer.jsx
+++ src/components/ecosystems/Main/MainContainer.jsx
@@ -1,7 +1,7 @@
 // Containers should include all logic that enhances a component
 // this includes any reduce methods, recompose, or middleware.
 
-import { compose, lifecycle, withStateHandlers } from 'recompose'
+import { compose, withStateHandlers, withHandlers, withPropsOnChange } from 'recompose'
 import { withEither } from '@bowtie/react-utils'
 import Main from './Main'
 import { Loading } from 'atoms'
@@ -14,28 +14,32 @@ export const enhance = compose(
   withStateHandlers(({ queryParams }) => ({
     repoList: [],
     repo: {},
-    isMainLoading: false
+    isMainLoading: false,
+    pageCount: 0
   }), {
-    setRepoList: ({ repoList }) => (payload) => ({ repoList: payload }),
-    setRepo: ({ repo }) => (payload) => ({ repo: payload }),
-    setCollections: ({ collections }) => (payload) => ({ collections: payload }),
-    setStagedFiles: ({ stagedFiles }) => (payload) => ({ stagedFiles: payload }),
-    setBranchList: ({ branchList }) => (payload) => ({ branchList: payload }),
-    setBranch: ({ branch }) => (payload) => ({ branch: payload }),
-    setMainLoading: ({ isMainLoading }) => (payload) => ({ isMainLoading: payload })
-  }),
-  lifecycle({
-    componentWillMount () {
-      const { setRepoList, match, setMainLoading } = this.props
-      const { model } = match.params
+      setRepoList: ({ repoList }) => (payload) => ({ repoList: payload }),
+      setRepo: ({ repo }) => (payload) => ({ repo: payload }),
+      nextPage: ({ pageCount }) => () => ({ pageCount: pageCount + 1 }),
+      previousPage: ({ pageCount }) => () => ({ pageCount: pageCount - 1 }),
+      setCollections: ({ collections }) => (payload) => ({ collections: payload }),
+      setStagedFiles: ({ stagedFiles }) => (payload) => ({ stagedFiles: payload }),
+      setBranchList: ({ branchList }) => (payload) => ({ branchList: payload }),
+      setBranch: ({ branch }) => (payload) => ({ branch: payload }),
+      setMainLoading: ({ isMainLoading }) => (payload) => ({ isMainLoading: payload })
+    }),
+  withHandlers({
+    getRepos: ({ pageCount, setMainLoading, setRepoList }) => () => {
       setMainLoading(true)
-      api.get(`${model}?sort=updated`)
+      api.get(`repos?page=${pageCount}&sort=updated`)
         .then(({ data }) => {
           setRepoList(data.repos)
           setMainLoading(false)
         })
         .catch(notifier.bad.bind(notifier))
     }
+  }),
+  withPropsOnChange(['pageCount'], ({ pageCount, getRepos }) => {
+    getRepos()
   }),
   withEither(loadingConditionFn, Loading)
 )
