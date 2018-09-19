e02b590f68d29e5c8ff6e60c242f2a9edae4d3f1
Tim Brandle <tim@bowtie.co>
2018-09-19 17:30:11 -0600

    update Field components

--- src/components/molecules/Field/FieldContainer.jsx
+++ src/components/molecules/Field/FieldContainer.jsx
@@ -3,37 +3,33 @@
 
 import { compose } from 'recompose'
 import { withEither } from '@bowtie/react-utils'
-import { endsWith } from '@bowtie/utils'
 import Input from './Input'
+import PhoneInput from './PhoneInput'
 import Select from './Select'
 import Checkbox from './Checkbox'
-import DateTimeContainer from './DateTime'
-import ColorPicker from './ColorPicker'
-import FileUpload from './FileUpload'
+import DatePicker from './DatePicker'
+import TimePicker from './TimePicker'
 import MultiSelect from './MultiSelect'
 import Radio from './Radio'
-import DocumentContainer from './Document/DocumentContainer'
+import ImageUpload from './ImageUpload'
 
 // const buttonCondition = ({ type }) => type === 'button' || type === 'submit'
-
-// const nullConditionFn = ({ name }) => !name;
-
-const isFileConditionFn = ({ type, name }) => endsWith(name, '_path') || type === 'file'
-const isColorConditionFn = ({ type, name }) => endsWith(name, '_color') || type === 'color'
-const dateTimePickerCondition = ({ type, name }) => endsWith(name, ['_date', '_on', '_time', '_at']) || type === 'date' || type === 'time' || type === 'datetime'
-const checkboxCondition = ({ type, name, value }) => typeof value === 'boolean' || endsWith(name, '_boolean') || type === 'checkbox'
-const radioCondition = ({ type }) => type === 'radio'
 const selectCondition = ({ type }) => type === 'select'
+const phoneCondition = ({ type }) => type === 'phone'
+const checkboxCondition = ({ type }) => type === 'checkbox'
+const datePickerCondition = ({ type }) => type === 'datepicker'
+const timePickerCondition = ({ type }) => type === 'timepicker'
 const multiSelectCondition = ({ type }) => type === 'multiselect'
-const documentUploadCondition = ({ type }) => type === 'document'
+const radioCondition = ({ type }) => type === 'radio'
+const imageUploadCondition = ({ type }) => type === 'image'
 
 export default compose(
   withEither(selectCondition, Select),
+  withEither(phoneCondition, PhoneInput),
   withEither(checkboxCondition, Checkbox),
-  withEither(dateTimePickerCondition, DateTimeContainer),
+  withEither(datePickerCondition, DatePicker),
+  withEither(timePickerCondition, TimePicker),
   withEither(multiSelectCondition, MultiSelect),
   withEither(radioCondition, Radio),
-  withEither(isColorConditionFn, ColorPicker),
-  withEither(isFileConditionFn, FileUpload),
-  withEither(documentUploadCondition, DocumentContainer)
+  withEither(imageUploadCondition, ImageUpload)
 )(Input)
--- src/components/molecules/Field/MultiSelect/MultiSelect.jsx
+++ src/components/molecules/Field/MultiSelect/MultiSelect.jsx
@@ -1,45 +1,23 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
 import { FormGroup } from 'atoms'
 
-const MultiSelect = ({ id, name, value, async, onChange, options, ...rest }) => {
-  const { setFormData, setStagedFileUploads, stagedFileUploads, fileUploads, ...sanitizedProps } = rest
-
+const MultiSelect = ({ id, name, async, onChange, className = '', edited, value = [], ...rest }) => {
   const SelectComponent = async ? Select.Async : Select
-
-  if (options && Array.isArray(options) && options.length > 0 && typeof options[0] !== 'object') {
-    options = options.reduce((arr, option) => {
-      const value = typeof option === 'object' ? (option.value || option.id) : option
-      const text = typeof option === 'object' ? (option.text || option.name) : option
-
-      return [ ...arr, { label: text, value } ]
-    }, [])
-  }
-
   return (
-    <FormGroup id={id} {...sanitizedProps}>
+    <FormGroup id={id} className={`${className} ${edited ? 'success-highlight' : ''}`} {...rest}>
       <SelectComponent
-        id={id}
         multi
         backspaceRemoves
-        value={value || []}
-        onChange={(value) => onChange({ target: { name, value } })}
-        options={options}
-        {...sanitizedProps}
+        id={id}
+        inputProps={{ autoComplete: 'nope' }}
+        value={value}
+        onChange={(v) => onChange({ target: { name, value: v } })}
+        {...rest}
       />
     </FormGroup>
   )
-}
-
-MultiSelect.propTypes = {
-  id: PropTypes.string,
-  name: PropTypes.string,
-  value: PropTypes.string,
-  async: PropTypes.bool,
-  onChange: PropTypes.func,
-  options: PropTypes.array
 }
 
 export default MultiSelect
