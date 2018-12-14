1c2e6a80e3992ab57b016da6f521a5084b5b05a6
Tim Brandle <tim@bowtie.co>
2018-12-14 12:27:26 -0700

    linting

--- src/components/environments/App/AppContainer.jsx
+++ src/components/environments/App/AppContainer.jsx
@@ -1,10 +1,9 @@
 // Containers should include all logic that enhances a component
 // this includes any reduce methods, recompose, or middleware.
 
-import qs from 'qs'
 import App from './App'
 import { withRouter } from 'react-router'
-import { compose, withPropsOnChange } from 'recompose'
+import { compose } from 'recompose'
 import { withEither } from '@bowtie/react-utils'
 import { Loading } from 'atoms'
 import { withQueryParams } from 'helpers'
