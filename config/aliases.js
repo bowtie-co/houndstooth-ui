ceaf17cacb9a799cb609d5ba721ae11ad2f3c153
Charlie McClung <charlie@bowtie.co>
2018-12-17 12:40:57 -0700

    WIP, pending another patch from CRA

--- config/aliases.js
+++ config/aliases.js
@@ -1,20 +1,28 @@
+const fs = require('fs')
 const path = require('path')
-// const paths = require('./paths')
+const basePath = path.join(__dirname, '..', 'src')
 
-module.exports = {
-  // Support React Native Web
-  // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-  'react-native': 'react-native-web',
-  'atoms': path.resolve(__dirname, '../src/components/atoms'),
-  'ecosystems': path.resolve(__dirname, '../src/components/ecosystems'),
-  'environments': path.resolve(__dirname, '../src/components/environments'),
-  'molecules': path.resolve(__dirname, '../src/components/molecules'),
-  'organisms': path.resolve(__dirname, '../src/components/organisms'),
-  'components': path.resolve(__dirname, '../src/components'),
-  'helpers': path.resolve(__dirname, '../src/helpers'),
-  'images': path.resolve(__dirname, '../src/images'),
-  'lib': path.resolve(__dirname, '../src/lib'),
-  'Routes': path.resolve(__dirname, '../src/Routes'),
-  'scss': path.resolve(__dirname, '../src/scss'),
-  'root': path.resolve(__dirname, '../src')
+const buildAliases = (fromPaths) => {
+  const aliases = {}
+  const pathList = Array.isArray(fromPaths) ? fromPaths : [ fromPaths ]
+
+  pathList.forEach(fromPath => {
+    const fullFromPath = path.join(basePath, fromPath)
+
+    const srcFiles = fs.readdirSync(fullFromPath)
+
+    console.log(srcFiles)
+
+    srcFiles.forEach(fileName => {
+      const filePath = path.join(fullFromPath, fileName)
+
+      if (fs.statSync(filePath).isDirectory()) {
+        aliases[fileName] = filePath
+      }
+    })
+  })
+
+  return aliases
 }
+
+module.exports = buildAliases([ '.', 'components' ])
---
+++ config/jest.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const aliases = require('./aliases')
+const basePath = path.join(__dirname, '..', 'src')
+
+const mapAliasNames = (aliases) => {
+  const nameMap = {}
+
+  Object.keys(aliases).forEach(alias => {
+    const aliasPath = aliases[alias]
+
+    const mappedPath = aliasPath.replace(basePath, '<rootDir>/src')
+
+    nameMap[`^${alias}[/]?(.*)`] = mappedPath + '/$1'
+  })
+
+  return nameMap
+}
+
+module.exports = {
+  browser: true,
+  moduleNameMapper: Object.assign({
+    '^libphonenumber-js$': 'libphonenumber-js'
+  }, mapAliasNames(aliases))
+}
