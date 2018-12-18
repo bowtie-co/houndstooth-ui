71994bc70b341cbb749a48c5b4acfb1af12a233f
Tim Brandle <tim@bowtie.co>
2018-12-18 12:19:38 -0700

    add .svg files to DirCard

--- src/components/molecules/DirCard/DirCard.jsx
+++ src/components/molecules/DirCard/DirCard.jsx
@@ -4,20 +4,17 @@ import { Link } from 'react-router-dom'
 import {
   Subtitle
 } from 'atoms'
-import * as images from 'images/fileImages'
 
 const DirCard = ({ dir, branch }) => {
   const supportedIcons = ['css', 'html', 'js', 'json']
   const { type, path, name } = dir
   const nameArray = name.split('.')
   const ext = nameArray.length > 1 ? nameArray[nameArray.length - 1] : null
-  const img = images ? images[supportedIcons.includes(ext) ? ext : type] : ''
-
   return (
     <Link
       to={{ pathname: `${type}`, search: `?path=${path}&ref=${branch}` }}
       className='repoDir'>
-      <img className='fileIcon' src={img} alt='' />
+      <img className='fileIcon' src={require(`./${supportedIcons.includes(ext) ? ext : type}.svg`)} alt='' />
       <Subtitle title={name} />
     </Link>
   )
