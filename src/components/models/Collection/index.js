fabe5c9222f05c8b4e44c4b7a3d6c6d6cf544e45
Tim Brandle <tim@bowtie.co>
2018-12-18 12:40:31 -0700

    remove files and folders unused

--- src/components/models/Collection/index.js
+++
@@ -1,7 +0,0 @@
-
-import Form from './Form'
-import List from './List'
-import Single from './Single'
-import Item from './Item'
-
-export { Form, List, Item, Single }
--- src/components/models/index.js
+++
@@ -1,4 +0,0 @@
-
-import * as Collection from './Collection'
-
-export { Collection }
