5f6ed21f8d37fd1d9cdef32d75514afc1c66ca84
Tim Brandle <tim@bowtie.co>
2019-01-03 16:10:16 -0700

    style changes

--- src/components/organisms/FileSingle/FileSingle.jsx
+++ src/components/organisms/FileSingle/FileSingle.jsx
@@ -1,18 +1,18 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Card, Button, TextEditor } from 'atoms'
+import { Button, TextEditor } from 'atoms'
 
 const FileSingle = ({ file, content, handleContentChange, saveFile }) => {
   return (
-    <Card>
+    <div style={{ 'padding': '20px' }}>
       <TextEditor
         content={content}
         name={file.name}
         onChange={handleContentChange}
       />
       <Button onClick={() => saveFile(content)} >Save</Button>
-    </Card>
+    </div>
   )
 }
 
