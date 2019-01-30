da7c6214cf959158997764729ee91e983db6cc4d
Tim Brandle <tim@bowtie.co>
2019-01-30 13:25:13 -0700

    remove console.log

--- src/components/atoms/Link/Link.jsx
+++ src/components/atoms/Link/Link.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { Link as LinkRR } from 'react-router-dom'
 
 const Link = ({ to, children, className }) => {
-  console.log('====================================')
-  console.log('link className', className)
-  console.log('====================================')
   return (
     <LinkRR className={`link ${className || ''}`} to={to || ''}>
       { children }
