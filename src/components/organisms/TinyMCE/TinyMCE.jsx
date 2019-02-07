c171fdf226b1880097618c0aff47657c45193938
Charlie McClung <charlie@cmr1.com>
2019-02-07 09:36:36 -0700

    Cleanup TinyMCE uploading handler

--- src/components/organisms/TinyMCE/TinyMCE.jsx
+++ src/components/organisms/TinyMCE/TinyMCE.jsx
@@ -2,41 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Editor } from '@tinymce/tinymce-react'
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
-import { api } from 'lib'
 
 const TinyMCE = (props) => {
-  const { baseRoute, queryParams, editorContent, onChange, baseApiRoute } = props
+  const { editorContent, onChange, onUpload } = props
 
   return (
     <div className='wysiwyg-section'>
       <Editor
-        // inline
+        inline
         init={{
           height: '100%',
-          plugins: 'link image code media hr lists fullscreen',
+          plugins: 'link image code hr lists fullscreen',
           menubar: 'false',
           toolbar1: 'undo redo | fontselect fontsizeselect | link media image',
           toolbar2: 'formatselect bold italic underline superscript hr | alignleft aligncenter alignright | bullist | removeformat | fullscreen code',
           content_style: 'img {max-width: 100%;}',
           // file_picker_types: 'file image media',
-          images_upload_handler: (blobInfo, success, failure) => {
-            console.log(Object.keys(blobInfo), blobInfo.id(), blobInfo.filename(), blobInfo.blobUri(), blobInfo.uri())
-            console.log('handle blob', blobInfo.name(), blobInfo.base64())
-
-            const body = {
-              files: [{
-                path: blobInfo.filename(),
-                content: blobInfo.base64(),
-                encoding: 'base64'
-              }],
-              message: 'File Upload'
-            }
-
-            api.post(`${baseApiRoute}/files/upsert`, body).then(resp => {
-              const fileUrl = `https://raw.githubusercontent.com/${baseRoute}/${queryParams['ref'] || 'master'}/${blobInfo.filename()}`
-              success(fileUrl)
-            }).catch(failure)
-          }
+          images_upload_handler: onUpload
         }}
         apiKey={'kpkfdogbexo2stz9zphjxq28zit6i9tch0ox4vlu1meo75hn'}
         value={editorContent}
--- src/components/organisms/TinyMCE/TinyMCEContainer.jsx
+++ src/components/organisms/TinyMCE/TinyMCEContainer.jsx
@@ -1,6 +1,7 @@
 
 import { compose, withState, withHandlers, withPropsOnChange } from 'recompose'
 import TinyMCE from './TinyMCE'
+import { api, notifier } from 'lib'
 // import htmlToDraft from 'html-to-draftjs'
 // import draftToHtml from 'draftjs-to-html'
 
@@ -12,6 +13,30 @@ export default compose(
     onChange: ({ handleEditorChange, setEditorContent }) => (newContent) => {
       handleEditorChange(newContent)
       setEditorContent(newContent)
+    },
+    onUpload: ({ baseRoute, baseApiRoute, queryParams }) => (blobInfo, success, failure) => {
+      console.log(Object.keys(blobInfo), blobInfo.id(), blobInfo.filename(), blobInfo.blobUri(), blobInfo.uri())
+      console.log('handle blob', blobInfo.name(), blobInfo.base64())
+
+      const file = {
+        path: `upload/${blobInfo.filename()}`,
+        content: blobInfo.base64(),
+        encoding: 'base64'
+      }
+
+      const body = {
+        files: [ file ],
+        message: `Upload file: ${file.path}`
+      }
+
+      api.post(`${baseApiRoute}/files/upsert`, body).then(resp => {
+        const fileUrl = `https://raw.githubusercontent.com/${baseRoute}/${queryParams['ref'] || 'master'}/${file.path}`
+        notifier.success(`Uploaded file: ${file.path}`)
+        success(fileUrl)
+      }).catch(err => {
+        notifier.bad(err)
+        failure(err)
+      })
     }
   }),
   withPropsOnChange(['content'], ({ content, setEditorContent }) => {
