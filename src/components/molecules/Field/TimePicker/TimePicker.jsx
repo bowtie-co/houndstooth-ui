29cbc13719998263246c9b21a7a9e0c22c7b2480
Tim Brandle <tim@bowtie.co>
2019-02-11 17:56:19 -0700

    remove DateTime component. remove react-datetime package

--- src/components/molecules/Field/TimePicker/TimePicker.jsx
+++ src/components/molecules/Field/TimePicker/TimePicker.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
-import TimePicker from 'rc-time-picker/lib'
-import moment from 'moment'
+import PropTypes from 'prop-types'
+import Datetime from 'react-datetime'
+import 'react-datetime/css/react-datetime.css'
 import { FormGroup } from 'atoms'
-import 'rc-time-picker/assets/index.css'
 
-const TimePickerField = ({ name, value, onChange, className = '', edited, ...rest }) => {
-  const selectedValue = typeof value === 'string' ? moment(value) : value
-
+const TimePickerField = (props) => {
+  const { value, onDateTimeChange, onChange, ...rest } = props
   return (
-    <FormGroup className={`${className} ${edited ? 'success-highlight' : ''}`} {...rest}>
-      <TimePicker
-        showSecond={false}
-        value={selectedValue ? moment.utc(selectedValue).local() : selectedValue}
-        onChange={(value) => onChange({ target: { name, value } })}
-        use12Hours
-        allowEmpty
+    <FormGroup floatLabel {...rest}>
+      <Datetime
+        value={value || ''}
+        inputProps={{ placeholder: rest['name'] }}
+        timeFormat
+        dateFormat={false}
+        onChange={(event) => onChange({ target: { value: event ? event.format('hh:mm A') : event } })}
         {...rest}
       />
     </FormGroup>
   )
+}
+
+TimePickerField.propTypes = {
+  value: PropTypes.string,
+  name: PropTypes.string,
+  onDateTimeChange: PropTypes.func
 }
 
 export default TimePickerField
