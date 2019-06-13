9b2755a260b7cae80a3c95a8529d201a864a668f
Joe-Pena <joepena@protonmail.com>
2019-06-13 16:50:57 -0700

    enhancement/166551379/invert blue items (#52)
    * Undo changes from master merge
    
    * Remove blue hover from logout btn and upload btn
    
    * Remove double quote
    
    * Specify content-wrapper for select style overrides

--- src/components/molecules/Darkmode/DarkmodeContainer.jsx
+++ src/components/molecules/Darkmode/DarkmodeContainer.jsx
@@ -21,8 +21,20 @@ export default compose(
         cssStyles: `
         .content-wrapper { filter: invert(90%) contrast(120%) brightness(135%);}
         li.page-item { filter: invert(90%)}
+        a.nav-link:hover, a.nav-link.active, a.link, .side-menu-section .nav-link:hover, .side-menu-section .nav-link.active { color: #00caff !important; }
+        li.active.pointer.nav-item { color: #00caff; border-left: 3px #00caff solid !important;}
+        i#Tooltip-delete-item-icon { color: #00caff !important;}
+        div.content-wrapper div.Select-option.is-focused {background-color:  rgba(0,202,252,0.25) !important;}
+        div.content-wrapper div.Select-option.is-selected {background-color: rgba(0,202,252,0.4) !important;}
+        .Select-control:active {border-color: #00caff !important; box-shadow: 0 0 0 0.2rem rgba(0,202,252,0.25) !important;}
+        input.field-group .form-control:focus, .field-group .form-control:active { border-color: #00caff !important;}
+        .field-group .form-control:focus { border-color: #00caff !important; box-shadow: 0 0 0 0.2rem rgba(0,202,252,0.25)}
         .avatar, video, button, .body-template img, .file-editor { filter: invert(100%);}
         .loader {background-color: black; filter: invert(100%);}
+        .file-tree-map-section a.not-active { color: #aeb3b8 !important;}
+        a.link.bold.parent-link { color: #000 !important;}
+        a.logout-btn.nav-link:hover { color: #fff !important; }
+        .inputfile .file-upload:hover { color: #00caff !important; border: 1px solid #00caff !important;}
       `
       }
     }
